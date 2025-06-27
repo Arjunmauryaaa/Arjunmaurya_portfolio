1cd3d0f85f947c75e253ff62db8e999afe4bddf8
Arjunmauryaaa <e1062240086@timscdrmumbai.in>
2025-06-27 14:46:07 +0530

    Fix alias paths and update jsconfig for deployment

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@
 import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
 
-import HeroSection from "@/components/homepage/hero-section";
-import AboutSection from "@/components/homepage/about";
-import Experience from "@/components/homepage/experience";
-import Skills from "@/components/homepage/skills";
-import Projects from "@/components/homepage/projects";
-import Education from "@/components/homepage/education";
-import Blog from "@/components/homepage/blog";
-import ContactSection from "@/components/homepage/contact";
+import HeroSection from "@/components/homepage/hero-section/index.jsx";
+import AboutSection from "@/components/homepage/about/index.jsx";
+import Experience from "@/components/homepage/experience/index.jsx";
+import Skills from "@/components/homepage/skills/index.jsx";
+import Projects from "@/components/homepage/projects/index.jsx";
+import Education from "@/components/homepage/education/index.jsx";
+import Blog from "@/components/homepage/blog/index.jsx";
+import ContactSection from "@/components/homepage/contact/index.jsx";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
